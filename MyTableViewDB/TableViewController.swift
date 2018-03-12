88d12b402312a07c46481e70b36d751b0946e083
desktop <desktop@desktop.com>
2018-03-12 16:25:29 +0800

    table view demo

---
+++ MyTableViewDB/TableViewController.swift
@@ -0,0 +1,121 @@
+//
+//  TableViewController.swift
+//  MyTableViewDB
+//
+//  Created by KaiChieh on 12/03/2018.
+//  Copyright © 2018 KaiChieh. All rights reserved.
+//
+
+import UIKit
+
+class TableViewController: UITableViewController {
+    var dicRow = [String : Any?]()
+    var arrTable = [[String : Any?]]()
+    var currentRow = 0
+
+    // MARK: - func
+    @objc private func btnEditAction() {
+        tableView.isEditing = !tableView.isEditing
+        tableView.isEditing ? (self.navigationItem.leftBarButtonItem?.title = "Finish") : (self.navigationItem.leftBarButtonItem?.title = "Edit")
+    }
+    @objc private func btnAddAction() {
+    }
+
+    // MARK: - LifeCycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        self.navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Edit", style: .plain, target: self , action: #selector(btnEditAction))
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Add", style: .plain, target: self, action: #selector(btnAddAction) )
+
+        //test data
+        dicRow["no"] = "S101"
+        dicRow["name"] = "番茄"
+        dicRow["gender"] = 1
+        dicRow["picture"] = UIImage(named: "DefaultPhoto.jpg")
+        dicRow["phone"] = "8825252"
+        dicRow["address"] = "花見小路"
+        dicRow["email"] = "test@yvtc.edu.tw"
+        dicRow["class"] = "WEB"
+        arrTable.append(dicRow)
+
+        dicRow["no"] = "S102"
+        dicRow["name"] = "番茄2"
+        dicRow["gender"] = 1
+        dicRow["picture"] = UIImage(named: "DefaultPhoto.jpg")
+        dicRow["phone"] = "8825252"
+        dicRow["address"] = "花見小路"
+        dicRow["email"] = "test@yvtc.edu.tw"
+        dicRow["class"] = "WEB"
+        arrTable.append(dicRow)
+
+        dicRow["no"] = "S103"
+        dicRow["name"] = "番茄3"
+        dicRow["gender"] = 1
+        dicRow["picture"] = UIImage(named: "DefaultPhoto.jpg")
+        dicRow["phone"] = "8825252"
+        dicRow["address"] = "花見小路"
+        dicRow["email"] = "test@yvtc.edu.tw"
+        dicRow["class"] = "WEB"
+        arrTable.append(dicRow)
+
+        tableView.refreshControl = UIRefreshControl()
+        tableView.refreshControl?.addTarget(self, action: #selector(handleRefresh), for: .valueChanged)
+        tableView.refreshControl?.attributedTitle = NSAttributedString(string: "Updating")
+    }
+    @objc private func handleRefresh() {
+        tableView.reloadData()
+        tableView.refreshControl?.endRefreshing()
+    }
+
+    // MARK: - Table view data source
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return arrTable.count
+    }
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MyCell", for: indexPath)
+
+        cell.textLabel?.text = arrTable[indexPath.row]["no"] as? String
+        cell.detailTextLabel?.text = arrTable[indexPath.row]["name"] as? String
+//        cell.accessoryType = .disclosureIndicator
+        return cell
+    }
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+//        print(arrTable[indexPath.row]["name"] as? String)
+        currentRow = indexPath.row
+    }
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    override func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
+        // when move, change my array
+        let tmp = arrTable.remove(at: sourceIndexPath.row)
+        arrTable.insert(tmp, at: destinationIndexPath.row)
+    }
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        arrTable.remove(at: indexPath.row)
+        tableView.deleteRows(at: [indexPath], with: .automatic )
+    }
+    override func tableView(_ tableView: UITableView, titleForDeleteConfirmationButtonForRowAt indexPath: IndexPath) -> String? {
+        return "老爺會看到"
+    }
+
+    // MARK: - prepare for segue
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let identifier = segue.identifier {
+            switch identifier {
+            case "Show":
+                if  let seguedToMVC = segue.destination as? ViewControllerDetail {
+                    seguedToMVC.tableViewController = self
+                }
+            default: break
+            }
+        }
+    }
+}
---
+++ MyTableViewDB/ViewControllerDetail.swift
@@ -0,0 +1,70 @@
+//
+//  ViewControllerDetail.swift
+//  MyTableViewDB
+//
+//  Created by KaiChieh on 12/03/2018.
+//  Copyright © 2018 KaiChieh. All rights reserved.
+//
+
+import UIKit
+
+class ViewControllerDetail: UIViewController {
+
+    weak var tableViewController: TableViewController!
+    var currentButtonYPosition: CGFloat = 0
+    var pkvGender: UIPickerView!
+    var pkvClass: UIPickerView!
+    let arrGender = ["F", "M"]
+    let arrClass = ["mobile Phone", "Web design", "IOT develope"]
+    var dicRow = [String : Any?]() {  // single row, sync to screen
+        didSet{
+            //            dicRow = arrTable[0] // get a dictionary from array dictionary
+            txtNo.text = dicRow["no"] as? String
+            txtName.text = dicRow["name"] as? String
+            txtGender.text = ((dicRow["gender"] as? Int) == 0) ? "女" : "難"
+//            if let aPic = dicRow["picture"] as? UIImage {
+//                imgPicture.image = aPic
+//            }
+            imgPicture.image = dicRow["picture"] as? UIImage
+            textPhone.text = dicRow["phone"] as? String
+            textClass.text = dicRow["class"] as? String
+            textAddress.text = dicRow["address"] as? String
+            textEmail.text = dicRow["email"] as? String
+        }
+    }
+
+    // MARK: - IBOutlet
+    @IBOutlet weak var txtNo: UITextField!
+    @IBOutlet weak var txtName: UITextField!
+    @IBOutlet weak var txtGender: UITextField!
+    @IBOutlet weak var imgPicture: UIImageView!
+    @IBOutlet weak var textPhone: UITextField!
+    @IBOutlet weak var textClass: UITextField!
+    @IBOutlet weak var textAddress: UITextField!
+    @IBOutlet weak var textEmail: UITextField!
+    @IBOutlet var textOutletCollection: [UITextField]!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        dicRow = tableViewController.arrTable[tableViewController.currentRow]
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
