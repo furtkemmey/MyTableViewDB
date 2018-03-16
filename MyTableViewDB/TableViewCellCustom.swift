28e15a6bd1c9b599363f8662fc4b139a210fc9b3
desktop <desktop@desktop.com>
2018-03-16 08:49:55 +0800

    add custom cell

---
+++ MyTableViewDB/TableViewCellCustom.swift
@@ -0,0 +1,24 @@
+//
+//  TableViewCellCustom.swift
+//  MyTableViewDB
+//
+//  Created by KaiChieh on 16/03/2018.
+//  Copyright © 2018 KaiChieh. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCellCustom: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
